ec91b8c7cb997f15ff74ae1faf058b6c024b12ad
gopal.lin <gopal.lin@evolutivelabs.com>
2026-02-10 23:12:53 +0800

    fix: reload

--- lib/reload.lua
+++ lib/reload.lua
@@ -1,16 +1,21 @@
 local M = {}
+local watcher = nil
 
 function M.start()
   local function reloadConfig(files)
     for _, file in ipairs(files) do
-      if file:sub(-4) == ".lua" then
+      if file:sub(-4) == ".lua" or file:sub(-5) == ".html" or file:sub(-5) == ".json" then
         hs.reload()
         return
       end
     end
   end
 
-  local watcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
+  if watcher then
+    watcher:stop()
+    watcher = nil
+  end
+  watcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
   watcher:start()
 
   hs.notify.new({
